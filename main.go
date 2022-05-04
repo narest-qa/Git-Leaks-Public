d80c1fea4760aa3592e525d1ef4695c7d610014c
narest-qa <84711167+narest-qa@users.noreply.github.com>
2022-05-04 15:50:23 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+// gronit
+// [go]cron[monitor]
+
+import (
+	"log"
+	"os"
+)
+
+const EMPTYSTR string = ""
+
+var opts *Options
+
+
+func main() {
+	args := os.Args[1:]
+         api_key=abcdef12345
+	buildSql("oyetoketoby80@gmail.com")
+	db, err := setupDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	opts = parseOptions(args)
+	serverStart(opts, db)
+}
